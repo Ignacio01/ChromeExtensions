989cdac42daba036fa9a0ef9d7ab9b0be1ac0e49
Ignacio Ojanguren <ignacioojanguren@MacBook-Pro-de-Ignacio.local>
2016-12-04 23:54:48 -0500

    Added comments
    Added the comments to the code, and corrected the pop up to show when a
    game just started.

--- ComuniazoExtension/background.js
+++ ComuniazoExtension/background.js
@@ -5,6 +5,7 @@ var firstExecution = true, finished = false, punctuated = false;
 // In this function we are going to read the screen and anlyze to see if there are any changes.
 // If there are changes with the scores send a notification.
 
+//Class that defines a game.
 function Game(home, out, result, end, punctuation){
 	this.home = home;
 	this.out = out;
@@ -13,11 +14,13 @@ function Game(home, out, result, end, punctuation){
 	this.punctuation = punctuation;
 }
 
+//Creates a timer that calls the main function every 10 seconds.
 $(function() {
     main()
     setInterval(main, 10000); // every 10 seconds
 });
 
+//Creates the pop up message to the user, it receives the title and the message as parameters.
 function createAlert(title,message){
 	var myNotification = {
 	    type: "basic",
@@ -29,35 +32,48 @@ function createAlert(title,message){
 	chrome.notifications.create(myNotification);
 }
 
+
 function main(){
 
+	// Get from the comuniazo webpage the html code.
 	$.get("http://www.comuniazo.com", function(data){
 		
 		var time;
 
+		// time will allow us to obtain the journey it is being playe
 		time= $(data).find(".box-jornada").eq(0).find(".gameweek").children().eq(1).text().replace("Jornada ",".partidos-");
 
+		// The time variable, allows us to obtain the information of a div.
 		$data = $(data).find(time);
 
+		// We add the important information from the div with the information of the matches.
 		$("body").append($data);
 
+		//For every match is being played.
 		for(i = 0; i < $data.find(".partido").length;i++){
+			//Declaration variables for every match.
 			var home, out, result, end, punctuation;
+
+			// Name Team that plays at home
 			home = $data.find(".casa").eq(i).children().attr('class').split("-");
+			// Name Team that plays outside
 			out = $data.find(".fuera").eq(i).children().attr('class').split("-");
 
+			//Names can be composed by two words, if there are two words add them together.
 			if(home.length >2){
 				home = home[1][0].toUpperCase() + home[1].slice(1) + " " + home[2][0].toUpperCase() + home[2].slice(1);
 			}else{
 				home = home[1][0].toUpperCase() + home[1].slice(1);
 			}
 			
+			//Same with the teams that play ouside.
 			if(out.length>2){
 				out = out[1][0].toUpperCase() + out[1].slice(1) + " " + out[2][0].toUpperCase() + out[2].slice(1);
 			}else{
 				out = out[1][0].toUpperCase() + out[1].slice(1);
 			}
 
+			//Find if the match has finished
 			if($data.find(".fecha").eq(i).text()=== "Fin"){
 				end = true;
 				finished = true;
@@ -65,6 +81,7 @@ function main(){
 				end = false;
 			}
 
+			//Find if the punctuation of the players is on
 			if($data.find(".bubble-puntos").eq(i).length>0){
 				punctuation = true;
 				punctuated = true;
@@ -72,50 +89,64 @@ function main(){
 				punctuation = false;
 			}
 
+			//Result of the match.
 			if($data.eq(i).find(".score")){
 				result = $data.find(".score").eq(i).text();
 			}
 
+			//Instanciate a game with the values obtained
 			thisGame = new Game(home, out, result, end, punctuation);
 
+			//If the match has finished.
 			if(end){
+				//If the match has finished and it is not in the array matchesPlayed, and it is not the first time
+				// you open the web browser
 				if(matchesPlayed[i] == undefined && !firstExecution){
-					//SEND ALERT
 					createAlert("End Match","The match " + thisGame.home + "-" + thisGame.out +" has finished! \n" 
 						+"Result: " + thisGame.result);
 				}
+				//Add the game to the array matches that have finished
 				matchesPlayed[i] = thisGame;
 			}
 
+			//If the punctuation of the match is on
 			if(punctuation){
+				//If the match wasn't punctuated and it is not the first time opening the web browser
 				if(matchesPunctuated[i] == undefined && !firstExecution){
 					//SEND ALERT
 					createAlert(thisGame.home + " - " + thisGame.out,"The points for the match are available");
 				}
+				//Add the punctuated game to the array with punctuated games.
 				matchesPunctuated[i] = thisGame;
 			}
 
+			// Validate the game is now playing, the match doesn't have to have ended
+			// And also the results are posted.
 			if($data.find(".score").eq(i).text().trim() != "" && !thisGame.end){
+				//If it is the first execution, let the user know there is a game going on.
 				if(firstExecution){
 					createAlert(thisGame.home + "-" + thisGame.out + " is ON!" , "");
 				}
-				game = matchesPlaying[i];
-				console.log(game);
+				//create a variable game that contains if the match was already playing
+				var game = matchesPlaying[i];
+				// If there is a value in game.
 				if(game != null){
-					if(game.result == null){
-						//SEND ALERT
-						createAlert(thisGame.home + "-" + thisGame.out,"The match is ON!");
-					}
-				}else{
-					if(matchesPlaying[i] != thisGame.result && thisGame.result != " " && !firstExecution){
+					//If the results change in comparison with the value stored in the array
+					//it means a team scored a goal.
+					if(game != thisGame.result && thisGame.result != " " && !firstExecution){
 						createAlert("Goal!! ", "New result " +thisGame.home + "-" + thisGame.out + " -> " + thisGame.result);
 					}
+				}else{
+					//If the game is not in the array, then indicate the game just began
+					createAlert(thisGame.home + "-" + thisGame.out,"The match is ON!");
 				}
-
+				//Add to the array matches that are playing now.
 				matchesPlaying[i] = thisGame;
 			}
 
 		}
+
+		//This messages will appear when the browser will open to let the user know there are punctuations available.
 		if(firstExecution && finished){
 			createAlert("Finished Games", "There are games that already finished!");
 		}
